05d8c2a8818f7ad401877981bb22601b3283867a
Kimleang Nao <kimleangn@gmail.com>
2020-01-05 14:07:14 -0800

    Updates

---
+++ precache-manifest.144e6910a3e0910b98126c39767382ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d57f3286cc26085050dd0a162ee951",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "128f36e3152780d38ca5",
+    "url": "/tictactoe/static/css/main.7d919cd1.chunk.css"
+  },
+  {
+    "revision": "6e04d9711c8a2eaa5da7",
+    "url": "/tictactoe/static/js/2.befbaaed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe/static/js/2.befbaaed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "128f36e3152780d38ca5",
+    "url": "/tictactoe/static/js/main.f22560c9.chunk.js"
+  },
+  {
+    "revision": "308602104fb55db98f27",
+    "url": "/tictactoe/static/js/runtime-main.2e200b3f.js"
+  }
+]);
